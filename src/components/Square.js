13e7bf91b1714b05f2e987846f374d2506a5121a
Shashank Singh <singhshashankcic@ducic.ac.in>
2020-09-26 18:25:17 +0000

    square component and destructuring props

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,5 +1,9 @@
 import React from 'react';
 
-const Square = () => <div>Square</div>
+const Square = ({ value, onClick }) => ( 
+  <button onClick={props.onClick}>
+    {props.value}
+  </button>
+);
 
 export default Square;
