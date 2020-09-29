52e2c18a6d1fda8611215c3118deb57de5227d34
Shashank Singh <singhshashankcic@ducic.ac.in>
2020-09-29 18:24:38 +0000

    Board Component and Scaffolding Functions

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const Square = ({ value, onClick }) => ( 
-  <button onClick={props.onClick}>
-    {props.value}
-  </button>
+const Square = ({ value, onClick }) => (
+    <button onClick={onClick}>
+      {value}
+    </button>
 );
 
 export default Square;
