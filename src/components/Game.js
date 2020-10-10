8ed66d4aef4df85994439291d7d78aea76bf921b
Shashank Singh <singhshashankcic@ducic.ac.in>
2020-10-10 18:14:28 +0000

    Create States and fill with initial data

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,23 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { calculateWinner } from '../helpers';
 import Board from './Board';
 
 const Game = () => {
-    
-    const handleClick = () => {
-        
-    }
-    
-    const jumpTo = () => {
-        
-    }
-    
-    const renderMoves = () => {
-        
-    }
-    
-    return (
-        <Board onClick={handleClick} />
-    )
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
+  const winner = calculateWinner(board);
+
+  const handleClick = () => {
+      
+  }
+  
+  const jumpTo = () => {
+      
+  }
+  
+  const renderMoves = () => {
+      
+  }
+  
+  return (
+      <Board squares={board} onClick={handleClick} />
+  )
 }
 
 export default Game;
