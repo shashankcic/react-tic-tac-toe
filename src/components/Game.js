f333bef52b1f47f43f239cd023d7c3e24fcdd76b
Shashank Singh <singhshashankcic@ducic.ac.in>
2020-10-10 20:39:20 +0000

    creating history

--- src/components/Game.js
+++ src/components/Game.js
@@ -8,36 +8,46 @@ const styles = {
 };
 
 const Game = () => {
-  const [board, setBoard] = useState(Array(9).fill(null));
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [stepNumber, setStepNumber] = useState(0);
   const [xIsNext, setXIsNext] = useState(true);
-  const winner = calculateWinner(board);
+  const winner = calculateWinner(history[stepNumber]);
 
   const handleClick = i => {
-    const boardCopy = [...board];
+    const timeInHistory = history.slice(0, stepNumber + 1);
+    const current = timeInHistory[stepNumber];
+    const squares = [...current];
     // If user clicks an occupied square or if game is won, return
-    if (winner || boardCopy[i]) return;
+    if (winner || squares[i]) return;
     // Put an X or an O in the clicked square
-    boardCopy[i] = xIsNext ? 'X' : 'O';
-    setBoard(boardCopy);
+    squares[i] = xIsNext ? 'X' : 'O';
+    setHistory([...timeInHistory, squares]);
+    setStepNumber(timeInHistory.length);
     setXIsNext(!xIsNext);
   }
   
-  const jumpTo = () => {
-      
-  }
+  const jumpTo = step => {
+    setStepNumber(step);
+    setXIsNext(step % 2 === 0)
+  };
   
   const renderMoves = () => (
-    <button onClick={() => setBoard(Array(9).fill(null))}>
-      Start Game
-    </button>
+    history.map((_step, move) => {
+      const destination = move ? `Go to move#${move}` : `Go to start`;
+      return (
+        <li key={move}>
+          <button onClick={() => jumpTo(move)}>{destination}</button>
+        </li>
+      );
+    })
   );
   
   return (
     <>
-      <Board squares={board} onClick={handleClick} />
+      <Board squares={history[stepNumber]} onClick={handleClick} />
       <div style={styles}>
         <p>{winner ? 'Winner: ' + winner : 'Next Player: ' + (xIsNext ? 'X' : 'O')}</p>
-        {renderMoves()};
+        {renderMoves()}
       </div>
     </>
   )
